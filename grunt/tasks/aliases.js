e1d4718cdccd223d04008a5d90544343332a238f
Aaron Sky <aaronsky@skyaaron.com>
2016-01-18 20:52:43 -0500

    Grunt config updates for jekyll workflow
    Former-commit-id: 88a65aa877856b1919c8d4e0f05b269992aec256

--- grunt/tasks/aliases.js
+++ grunt/tasks/aliases.js
@@ -4,26 +4,5 @@ module.exports = function(grunt) {
     'clean',
     'imagemin',
     'svgmin',
-    'copy:start',
-    'sass:dev',
-    'uncss',
-    //'inline',
-    'copy:final',
-    'connect',
-    'watch'
-  ]);
-
-  grunt.registerTask('build', [
-    'clean',
-    'imagemin',
-    'svgmin',
-    'copy:start',
-    'sass:build',
-    'uncss',
-    'cssmin',
-    'replace',
-    'clean:unminified',
-    'copy:final',
-    //,'inline'
   ]);
 }
