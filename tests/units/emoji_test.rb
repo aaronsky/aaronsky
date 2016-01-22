18cdc36f97109f8e5d9354717995ae4676db1c65
Aaron Sky <aaronsky@skyaaron.com>
2016-01-22 13:41:46 -0500

    Updated tests for nicer formatting

---
+++ tests/units/emoji_test.rb
@@ -0,0 +1,19 @@
+require_relative "../helpers/blog_namespace"
+
+require "open-uri"
+require "net/http"
+
+class EmojiTest < Blog::Test
+  def test_no_emoji
+    puts "Testing for emoji in #{posts.length} posts\n----------------------------\n"
+    posts.each do |post|
+      print "Processing #{post}......"
+      content_with_header = File.read(post)
+      index = content_with_header.index('---', content_with_header.index('---')+1)+5
+      content = content_with_header[index..-1]
+      refute_match(/:[a-zA-Z0-9_]+:/, content, "\nYou have emoji in your post, which we try to avoid")
+      puts "No problems found"
+    end
+    puts "------\n"
+  end
+end
