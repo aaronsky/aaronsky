d394dfa53455dbafaf57667eb18875d25e92d895
Aaron Sky <aaronsky@skyaaron.com>
2016-01-22 13:18:58 -0500

    Updated emoji test to actually test things
    Former-commit-id: 24a432b83ab2ee86601f82162b2774ff2836b610

--- tests/emoji_test.rb
+++ tests/emoji_test.rb
@@ -5,9 +5,12 @@ require "net/http"
 
 class EmojiTest < Blog::Test
   def test_no_emoji
+    puts "Running Emoji Tests against #{posts.length}"
     posts.each do |post|
+      puts "Processing #{post}"
       content_with_header = File.read(post)
-      content = content_with_header.slice(content_with_header.index('---', content_with_header.index('---')+1))
+      index = content_with_header.index('---', content_with_header.index('---')+1)+5
+      content = content_with_header[index..-1]
       refute_match(/:[a-zA-Z0-9_]+:/, content, "You have emoji in your post, which we try to avoid")
     end
   end
