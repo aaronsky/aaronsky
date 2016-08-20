f84b7abc598f56190f995b87e9a8d7074f115a57
Aaron Sky <aaronsky@skyaaron.com>
2016-08-20 08:43:31 -0400

    Added more logging to deploy script

--- .bin/deploy.rb
+++ .bin/deploy.rb
@@ -3,8 +3,9 @@ require 'json'
 require 'uri'
 require 'base64'
 
-exit if ENV['TRAVIS_TAG']?
-exit if ENV['TRAVIS_PULL_REQUEST']?
+if ENV['TRAVIS_TAG']? || ENV['TRAVIS_PULL_REQUEST']?
+  exit
+then
 
 hostname = 'https://api.github.com/'
 repo_slug = ENV['TRAVIS_REPO_SLUG']
@@ -14,6 +15,8 @@ username = ENV['CI_USERNAME']
 password = ENV['CI_GH_TOKEN']
 client = Net::HTTP.new(uri.hostname, uri.port)
 
+puts "Beginning deploy to #{uri}"
+
 get_request = Net::HTTP::Get.new(uri.request_uri)
 get_request.basic_auth username, password unless username.nil?
 update_request['Content'] = 'application/json'
@@ -21,9 +24,13 @@ get_response = client.request(get_request)
 get_response_body = JSON.parse(get_response.body)
 file_sha = get_response_body["sha"]
 
+puts "PDF file sha is #{file_sha}"
+
 pdf_file_path = "#{TRAVIS_BUILD_DIR}/_site/assets/docs/resume.pdf"
 pdf_file = File.open(pdf_file_path, 'rb')
 encoded_file = Base64.encode64(pdf_file)
+
+puts "PDF file successfully encoded"
 
 body = {
   message: "Update resume in repo after passing CI.\n\n[skip ci]",
@@ -41,4 +48,9 @@ update_request.basic_auth username, password unless username.nil?
 update_request['Content'] = 'application/json'
 update_request.body = body.to_json
 update_response = client.request(update_request)
+
 puts update_response.body
+
+if update_response.code != 200
+  puts "Update PDF file failed with a #{update_response.code} code"
+end
