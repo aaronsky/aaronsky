9d770053f0000e23bd075674fa5b3be3d4666fc0
Aaron Sky <aaronsky@skyaaron.com>
2016-08-20 09:22:21 -0400

    Added missing variables to deploy script
    Former-commit-id: 5bad3e593c2361c44c2d73221ca23bdadb2deb48

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
     exit 0
