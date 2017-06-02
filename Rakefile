7449b9ef3727186fae76d128e7f535524cae7e61
Aaron Sky <aaronsky@skyaaron.com>
2017-06-02 13:01:34 -0400

    Killed infinite rebuild loop in watch task

--- Rakefile
+++ Rakefile
@@ -1,6 +1,7 @@
+require 'fileutils'
+require 'html-proofer'
 require 'optparse'
 require 'rake/testtask'
-require 'html-proofer'
 
 task :default => [:help]
 task :rebuild => [:clean, :build]
@@ -17,10 +18,15 @@ end
 
 task :build do
   sh 'bundle exec jekyll build -- --verbose'
+  copy_resume_to_assets
 end
 
 task :serve do
-  trap('SIGINT') { puts "\nServer closed." ; exit }
+  trap('SIGINT') {
+    puts "\nServer closed."
+    copy_resume_to_assets
+    exit 
+  }
   sh 'bundle exec jekyll serve --skip-initial-build'
 end
 
@@ -30,9 +36,17 @@ task :proof do
     :check_html => true,
     :only_4xx => true,
     :check_favicon => true
-    }).run
+  }).run
 end
 
 task :integrations do
   ruby "tests/test_helper.rb"
+end
+
+def copy_resume_to_assets
+  current_dir = File.dirname __FILE__
+  site_assets_dir = File.join(current_dir, '_site', 'assets', 'docs')
+  project_assets_dir = File.join(current_dir, 'assets', 'docs')
+  target_file = File.join(site_assets_dir, 'resume.pdf')
+  FileUtils.cp(target_file, project_assets_dir, :verbose => true)
 end
--- _plugins/resume-pdf.rb
+++ _plugins/resume-pdf.rb
@@ -1,34 +1,32 @@
-require 'fileutils'
 require 'pdfkit'
 
 current_dir = File.dirname(__FILE__)
-site_dir = File.join(current_dir, '..', '_site')
-output_dir = File.join(site_dir, 'assets', 'docs')
-output_file = File.join(output_dir, 'resume.pdf')
-assets_output = File.join(current_dir, '..', 'assets', 'docs')
+project_root = File.join(current_dir, '..')
+assets_dir = File.join(project_root, 'assets')
+assets_docs_dir = File.join(assets_dir, 'docs')
+site_dir = File.join(project_root, '_site')
+site_assets_dir = File.join(site_dir, 'assets')
+site_assets_docs_dir = File.join(site_assets_dir, 'docs')
 
+output_file = File.join(site_assets_docs_dir, 'resume.pdf')
 pdf = nil
+scrubbed_html = nil
 
 PDFKit.configure do |config|
   config.wkhtmltopdf = ENV['WKHTMLTOPDF_CMD'] if ENV['CI']
   config.default_options = {
-    :page_size => 'Letter',
+    page_size: 'Letter'
   }
-  config.verbose = true
+  config.verbose = ENV.has_key? 'CI'
 end
 
-##
-# if Dir.exist? temp_dir
-#   FileUtils.rm_r temp_dir
-# end
-# Dir.mkdir temp_dir
+Jekyll::Hooks.register :pages, :post_write do |page|
+  next if page.name != 'resume.html'
+  scrubbed_html = page.output.gsub(/(href|src)=(['"])\/([^\"']*|[^"']*)['"]/, '\1=\2' + site_dir + '/\3\2')
+end
 
 Jekyll::Hooks.register :site, :post_write do |site|
-  page = site.pages.find { |page| page.name == "resume.html" }
-  if page
-    html = page.output.gsub(/(href|src)=(['"])\/([^\"']*|[^"']*)['"]/, '\1=\2' + site_dir + '/\3\2')
-    kit = PDFKit.new(html)
-    pdf = kit.to_file(output_file)
-    FileUtils.cp(output_file, assets_output, :verbose => true)
-  end
+  next if scrubbed_html.nil?
+  kit = PDFKit.new(scrubbed_html)
+  pdf = kit.to_file(output_file)
 end
