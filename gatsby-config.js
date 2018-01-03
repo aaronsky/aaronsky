f2d4002ada655cb43ffdf082f68fea77f1255cb1
Aaron Sky <asky@wayfair.com>
2018-01-02 19:10:12 -0500

    Updated Helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
     siteMetadata: {
         title: 'Portfolio',
         author: 'Aaron Sky',
+        caption: '📱 App 🌐 Web 🎮 Game',
         site: 'https://skyaaron.com',
         linkedin: 'https://www.linkedin.com/in/aaronsky/',
         github: 'https://github.com/aaronsky/',
